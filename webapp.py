e51498261602ab2783ca8ed2757fbab5d14c6a7a
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-03-03 10:02:46 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,6 +13,7 @@ def render_response():
     birthday = request.args['birthday']
     color = request.args['color'] 
     free = request.args['free']
+    vol = request.args['vol']
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
