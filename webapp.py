03e6301adbc8481bd00089b5e4c47f4d153c0a93
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-03-04 14:50:41 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,45 +18,44 @@ def render_response():
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if lname=="" or birthday =="" or color =="" or free=="":
-        reply = "Please fill out all of the information"
+        return render_template('response.html', response = "Please fill out every box of information")
+    month = birthday[5: 7]
+    if month == "01":
+        month = "Jan"
+    if month == "02":
+        month = "Feb"
+    if month == "03":
+        month = "Mar"
+    if month == "04":
+        month = "Apr"
+    if month == "05":
+        month = "May"
+    if month == "06":
+        month = "Jun"
+    if month == "07":
+        month = "Jul"
+    if month == "08":
+        month = "Aug"
+    if month == "09":
+        month = "Sep"
+    if month == "10":
+        month = "Oct"
+    if month == "11":
+        month = "Nov"
+    if month == "12":
+        month = "Dec"
+
+    last = random.randrange(0, 2)
+    if last == 0:
+        lname = lname[0:2]
     else:
-        month = birthday[5: 7]
-        if month == "01":
-            month = "Jan"
-        if month == "02":
-            month = "Feb"
-        if month == "03":
-            month = "Mar"
-        if month == "04":
-            month = "Apr"
-        if month == "05":
-            month = "May"
-        if month == "06":
-            month = "Jun"
-        if month == "07":
-            month = "Jul"
-        if month == "08":
-            month = "Aug"
-        if month == "09":
-            month = "Sep"
-        if month == "10":
-            month = "Oct"
-        if month == "11":
-            month = "Nov"
-        if month == "12":
-            month = "Dec"
-    
-        last = random.randrange(0, 2)
-        if last == 0:
-            lname = lname[0:2]
-        else:
-            lname = lname[0] + lname[-1]
+        lname = lname[0] + lname[-1]
         
-        mylist = [lname, month, color, free]
-        for x in mylist:
-            choice = random.choice(mylist)
-            reply = reply + choice
-            mylist.remove(choice)
+    mylist = [lname, month, color, free]
+    for x in mylist:
+        choice = random.choice(mylist)
+        reply = reply + choice
+        mylist.remove(choice)
     
     return render_template('response.html', response = reply)
     
