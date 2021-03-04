bf31cd4a47e6491c20e97c666812594de752ec39
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-03-04 09:45:56 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,6 +17,7 @@ def render_response():
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
+    month = birthday[5: 7]
     reply = lname + birthday + color + free + vol
     
     return render_template('response.html', response = reply)
