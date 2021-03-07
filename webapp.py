ceec3805333eb0bd0a2ebce9db24c1d2a6957610
Ponmo <77691125+Ponmo@users.noreply.github.com>
2021-03-06 22:37:49 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,8 +17,8 @@ def render_response():
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if lname=="" or birthday =="" or color =="" or free=="":
-        return render_template('response.html', response = "Please fill out every box of information")
+    if color =="" or free=="":
+        return render_template('response.html', response = "Please fill out all required boxes")
     month = birthday[5: 7]
     if month == "01":
         month = "Jan"
